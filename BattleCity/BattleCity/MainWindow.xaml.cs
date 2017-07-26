ceed9eec539f56341a60ece10534bc604bd7198c
Krzysztof Barszcz <krzysztof.barszcz.92@gmail.com>
2017-07-26 19:58:53 +0200

    Added reaction on keys at the main screen

--- BattleCity/BattleCity/MainWindow.xaml.cs
+++ BattleCity/BattleCity/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.Windows.Input;
 
 namespace BattleCity
 {
@@ -10,18 +11,22 @@ namespace BattleCity
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private Image background = new Image();
+		private Image pointer = new Image();
+		private Options selectedOption;
+
+		enum Options { ONE_PLAYER, TWO_PLAYERS, EDITOR}
 		public MainWindow()
 		{
 			InitializeComponent();
+			background.Source = new BitmapImage(new Uri("Images/startScreen.bmp", UriKind.Relative));
+			pointer.Source = new BitmapImage(new Uri("Images/icon.bmp", UriKind.Relative));
+			selectedOption = Options.ONE_PLAYER;
 		}
 
 		private void InitializeMainScreen()
 		{
-			Image background = new Image();
-			Image pointer = new Image();
-			background.Source = new BitmapImage(new Uri("Images/startScreen.bmp", UriKind.Relative));
 			mainCanvas.Children.Add(background);
-			pointer.Source = new BitmapImage(new Uri("Images/icon.bmp", UriKind.Relative));
 			Thickness margin = pointer.Margin;
 			margin.Top = 264;
 			margin.Left = 130;
@@ -32,6 +37,47 @@ namespace BattleCity
 		private void mainWindow_Loaded(object sender, RoutedEventArgs e)
 		{
 			InitializeMainScreen();
+		}
+
+		private void mainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key == Key.Down)
+			{
+				if (pointer.Margin.Top < 328)
+				{
+					MoveDown(32);
+					selectedOption = selectedOption == Options.ONE_PLAYER ? Options.TWO_PLAYERS : Options.EDITOR;
+				}
+			}
+			if (e.Key == Key.Up)
+			{
+				if (pointer.Margin.Top > 264)
+				{
+					MoveDown(-32);
+					selectedOption = selectedOption == Options.EDITOR ? Options.TWO_PLAYERS : Options.ONE_PLAYER;
+				}
+			}
+			if (e.Key == Key.Space)
+			{
+				switch (selectedOption)
+				{
+					case Options.ONE_PLAYER:
+						break;
+					case Options.TWO_PLAYERS:
+						break;
+					case Options.EDITOR:
+						break;
+				}
+			}
+		}
+
+		private void MoveDown(int pixels)
+		{
+			mainCanvas.Children.Clear();
+			mainCanvas.Children.Add(background);
+			Thickness margin = pointer.Margin;
+			pointer.Margin = new Thickness(130, margin.Top + pixels, 0, 0);
+			mainCanvas.Children.Add(pointer);
 		}
 	}
 }
