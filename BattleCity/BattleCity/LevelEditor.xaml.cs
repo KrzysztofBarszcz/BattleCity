6fa41677a365737c1ad0540d243af54a04ba8ccb
Krzysztof Barszcz <krzysztof.barszcz.92@gmail.com>
2017-07-26 23:01:39 +0200

    Add the list of available textures in the editor

--- BattleCity/BattleCity/LevelEditor.xaml.cs
+++ BattleCity/BattleCity/LevelEditor.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace BattleCity
 {
@@ -19,9 +11,42 @@ namespace BattleCity
 	/// </summary>
 	public partial class LevelEditor : Window
 	{
+		private enum Element {
+			Concrete,
+			Fast,
+			Tree,
+			Wall,
+			Water
+		}
+
+		private Dictionary<String, BitmapImage> textures = new Dictionary<String, BitmapImage>();
+	
+		private String pathToTextures = "Images/LevelElements/";
 		public LevelEditor()
 		{
+			PrepareTextures();
 			InitializeComponent();
+			texturesListBox.ItemsSource = textures.Keys;
+			//textureImage.Source = new BitmapImage(new Uri(pathToTextures + "Concrete.bmp", UriKind.Relative));
+			
+		}
+
+		private void PrepareTextures()
+		{
+			foreach (var e in Enum.GetNames(typeof(Element))){
+				Uri uri = new Uri(pathToTextures + e + ".bmp", UriKind.Relative);
+				var bitmap = new BitmapImage(uri);
+				/*var image = new Image();
+				image.Source = bitmap;*/
+				textures.Add(e, bitmap);
+			}
+		}
+
+		private void texturesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var listBox = sender as ListBox;
+			var key = listBox.SelectedItem as String;
+			textureImage.Source = textures[key];
 		}
 	}
 }
