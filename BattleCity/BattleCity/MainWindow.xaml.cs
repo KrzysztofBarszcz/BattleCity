57ff4aaa9504b654cd347186982f76496ad48bf6
Krzysztof Barszcz <krzysztof.barszcz.92@gmail.com>
2017-07-26 19:19:48 +0200

    Refactor of the main window

--- BattleCity/BattleCity/MainWindow.xaml.cs
+++ BattleCity/BattleCity/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BattleCity
 {
@@ -23,6 +13,25 @@ namespace BattleCity
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void InitializeMainScreen()
+		{
+			Image background = new Image();
+			Image pointer = new Image();
+			background.Source = new BitmapImage(new Uri("Images/startScreen.bmp", UriKind.Relative));
+			mainCanvas.Children.Add(background);
+			pointer.Source = new BitmapImage(new Uri("Images/icon.bmp", UriKind.Relative));
+			Thickness margin = pointer.Margin;
+			margin.Top = 264;
+			margin.Left = 130;
+			pointer.Margin = margin;
+			mainCanvas.Children.Add(pointer);
+		}
+
+		private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			InitializeMainScreen();
 		}
 	}
 }
