688218dd078e0b85e63332e6c0f0ac9bd2fe68c8
Krzysztof Barszcz <krzysztof.barszcz.92@gmail.com>
2017-08-06 17:18:42 +0200

    Add level serializer
    Similar operations have to be performed while loading level in the editor and
    in the main game
    It would be profitable to prepare helper serializer class

---
+++ BattleCity/BattleCity/Helpers/LevelSerializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using BattleCity.Elements;
+
+namespace BattleCity.Helpers
+{
+	public class LevelSerializer
+	{
+		private static LevelSerializer instance;
+
+		private DataContractSerializer serializer;
+		private LevelSerializer()
+		{
+			serializer = new DataContractSerializer(typeof(List<DrawableElement>), new List<Type> { typeof(Field) });
+		}
+
+		public static LevelSerializer GetInstance()
+		{
+			if(instance == null)
+			{
+				instance = new LevelSerializer();
+			}
+			return instance;
+		}
+
+		public void SerializeLevel(String path, List<DrawableElement> levelElements)
+		{
+			using (FileStream fileStream = new FileStream(path, FileMode.Create))
+			{
+				serializer.WriteObject(fileStream, levelElements);
+			}
+		}
+
+		public List<DrawableElement> DeserializeLevel(String path)
+		{
+			using (FileStream fileStream = new FileStream(path, FileMode.Open))
+			{
+				return serializer.ReadObject(fileStream) as List<DrawableElement>;
+			}
+		}
+	}
+}
