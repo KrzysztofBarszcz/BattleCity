f81736c43e8ef0060e731496d89a9348c989364a
Piotr <piotrbarszcz1992@gmail.com>
2017-08-06 21:38:30 +0200

    Add abstract tank class

---
+++ BattleCity/BattleCity/Elements/AbstractTank.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace BattleCity.Elements
+{
+    abstract class AbstractTank
+    {
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+        public double Speed { get; set; }
+        public double Resistance { get; set; }
+        public BitmapImage Image { get; set; }
+    }
+}
