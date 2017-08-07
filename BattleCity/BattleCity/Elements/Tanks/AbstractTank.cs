ad22c7d14ee58f8e5cb4ec0a34e26d295306e813
Krzysztof Barszcz <krzysztof.barszcz.92@gmail.com>
2017-08-07 12:48:13 +0200

    Introduce movement helper
    AbstractTank class grew too big. We can move movement methods to a helper
    class

--- BattleCity/BattleCity/Elements/Tanks/AbstractTank.cs
+++ BattleCity/BattleCity/Elements/Tanks/AbstractTank.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using BattleCity.Helpers;
 
 namespace BattleCity.Elements.Tanks
 {
@@ -32,63 +32,13 @@ namespace BattleCity.Elements.Tanks
 		{
 			if (IsMoving)
 			{
-				Move();
+				MovementHelper.GetInstance().Move(this);
 			}
 			var image = new Image();
 			image.Source = Image;
 			image.Margin = new Thickness(PositionX - 16, PositionY - 16, 0, 0);
-			image.RenderTransform = RotateTank();
+			image.RenderTransform = MovementHelper.GetInstance().RotateTank(this);
 			return image;
-		}
-
-		private RotateTransform RotateTank()
-		{
-			
-			switch (Direction)
-			{
-				case Direction.Up:
-					return null;
-				case Direction.Down:
-					return new RotateTransform(180,16,16);
-				case Direction.Left:
-					return new RotateTransform(270, 16, 16);
-				case Direction.Right:
-					return new RotateTransform(90, 16, 16);
-				default:
-					return null;
-			}
-		}
-
-		private void Move()
-		{
-			double delta = (double)Clock.ElapsedMilliseconds /1000;
-			Clock.Restart();
-			switch (Direction)
-			{
-				case Direction.Up:
-					PositionY -= delta * Speed;
-					PositionX = Trim(PositionX);
-					break;
-				case Direction.Down:
-					PositionY += delta * Speed;
-					PositionX = Trim(PositionX);
-					break;
-				case Direction.Left:
-					PositionX -= delta * Speed;
-					PositionY = Trim(PositionY);
-					break;
-				case Direction.Right:
-					PositionX += delta * Speed;
-					PositionY = Trim(PositionY);
-					break;
-			}
-		}
-
-		private int Trim(double position)
-		{
-			if (position % 16 >= 8)
-				return ((int)position / 16 + 1) * 16;
-			else return (int)position / 16 * 16;
 		}
 	}
 }
---
+++ BattleCity/BattleCity/Helpers/MovementHelper.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Media;
+using BattleCity.Elements.Tanks;
+
+namespace BattleCity.Helpers
+{
+	class MovementHelper
+	{
+		private static MovementHelper instance;
+
+		public static MovementHelper GetInstance()
+		{
+			if (instance == null)
+			{
+				instance = new MovementHelper();
+			}
+			return instance;
+		}
+
+		public void Move(AbstractTank tank)
+		{
+			double delta = (double)tank.Clock.ElapsedMilliseconds / 1000;
+			tank.Clock.Restart();
+			switch (tank.Direction)
+			{
+				case Direction.Up:
+					tank.PositionY -= delta * tank.Speed;
+					tank.PositionX = Trim(tank.PositionX);
+					break;
+				case Direction.Down:
+					tank.PositionY += delta * tank.Speed;
+					tank.PositionX = Trim(tank.PositionX);
+					break;
+				case Direction.Left:
+					tank.PositionX -= delta * tank.Speed;
+					tank.PositionY = Trim(tank.PositionY);
+					break;
+				case Direction.Right:
+					tank.PositionX += delta * tank.Speed;
+					tank.PositionY = Trim(tank.PositionY);
+					break;
+			}
+		}
+
+		private int Trim(double position)
+		{
+			if (position % 16 >= 8)
+				return ((int)position / 16 + 1) * 16;
+			else return (int)position / 16 * 16;
+		}
+
+		public RotateTransform RotateTank(AbstractTank tank)
+		{
+			switch (tank.Direction)
+			{
+				case Direction.Up:
+					return null;
+				case Direction.Down:
+					return new RotateTransform(180, 16, 16);
+				case Direction.Left:
+					return new RotateTransform(270, 16, 16);
+				case Direction.Right:
+					return new RotateTransform(90, 16, 16);
+				default:
+					return null;
+			}
+		}
+	}
+}
